50ac50a8c47b76cf49bd3004fe82de7ab6285644
Gab071 <kaminskigabriel07@gmail.com>
2026-09-09 15:51:05 +0300

    changed pin sequence

--- memoryGame/memoryGame.ino
+++ memoryGame/memoryGame.ino
@@ -4,14 +4,14 @@
 #include "pitches.h"
 
 // Hardware & Pin mapping
-const int LED_PINS[]    = { 12, 11, 10, 9 }; // Blue, Red, Yellow, Green
-const uint8_t BUTTON_PINS[] = { 7, 6, 5, 4 };    // Blue, Red, Yellow, Green
-const uint8_t BUZZER_PIN    = 8;
+const int LED_PINS[]    = {A0, A1, 3, 2}; // Blue, Red, Yellow, Green
+const uint8_t BUTTON_PINS[] = {13, 12, 11, 4};    // Blue, Red, Yellow, Green
+const uint8_t BUZZER_PIN    = A2;
 const int GAME_TONES[]     = { NOTE_C3, NOTE_E3, NOTE_G3, NOTE_C4 };
 const int numInputs = 4;
 const int SequenceLength = 6;
 
-LiquidCrystal lcd(13, 3, A0, A1, A2, 2);
+LiquidCrystal lcd(10, 9, 8, 7, 6, 5);
 
 Controller<SequenceLength> game(LED_PINS, BUTTON_PINS, BUZZER_PIN, numInputs, GAME_TONES, lcd);
 
